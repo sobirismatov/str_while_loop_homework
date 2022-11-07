a841ac1faea6cbda9c20a0eac5fec57f3fc49631
sobirismatov <name@uzcoder96@gmail.com>
2022-11-07 17:41:03 +0500

    10

--- While10.py
+++ While10.py
@@ -1,16 +1,16 @@
-# def main(s):
-#     """
-#     A string of numbers is given. Find and return the sum of all odd numbers.
-#     Args:
-#         s: str
-#     Returns:
-#         int: return answer
-#     """
-#     i=0
-#     k=0
-#     while i<len(s):
-#         if int(s[i])%2==1:
-#             k+=int(s[i])
-#         i+=1
-#     return k
-# print(main("12345"))
+def main(s):
+    """
+    A string of numbers is given. Find and return the sum of all odd numbers.
+    Args:
+        s: str
+    Returns:
+        int: return answer
+    """
+    i=0
+    k=0
+    while i<len(s):
+        if int(s[i])%2==1:
+            k+=int(s[i])
+        i+=1
+    return k
+print(main("12345"))
