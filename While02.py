11c85feaa9c1f9dcea8e38f4b8ac9d9f7065f1be
sobirismatov <name@uzcoder96@gmail.com>
2022-11-07 16:40:58 +0500

    02

--- While02.py
+++ While02.py
@@ -1,16 +1,16 @@
-# def main(s):
-#     """
-#     A variable of type str is given. Find how many letters it contains and return.
-#     Args:
-#         s: str
-#     Returns:
-#         int: return answer
-#     """
-#     i=0
-#     k=0
-#     while i<len(s):
-#         if s[i].isalpha():
-#             k+=1
-#         i+=1 
-#     return k
-# print(main("sal 11222m"))
+def main(s):
+    """
+    A variable of type str is given. Find how many letters it contains and return.
+    Args:
+        s: str
+    Returns:
+        int: return answer
+    """
+    i=0
+    k=0
+    while i<len(s):
+        if s[i].isalpha():
+            k+=1
+        i+=1 
+    return k
+print(main("sal 11222m"))
