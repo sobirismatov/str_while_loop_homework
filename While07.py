01161bf9b136bba4fce482449d5109c4ebe0f332
sobirismatov <name@uzcoder96@gmail.com>
2022-11-07 17:34:27 +0500

    07

--- While07.py
+++ While07.py
@@ -1,16 +1,16 @@
-# def main(s):
-#     """
-#     A string of numbers is given. Find how many even numbers there are and return.
-#     Args:
-#         s: str
-#     Returns:
-#         int: return answer
-#     """
-#     i=0
-#     k=0
-#     while i<len(s):
-#         if int(s[i])%2==0:
-#             k+=1
-#         i+=1
-#     return k
-# print(main("5234"))
+def main(s):
+    """
+    A string of numbers is given. Find how many even numbers there are and return.
+    Args:
+        s: str
+    Returns:
+        int: return answer
+    """
+    i=0
+    k=0
+    while i<len(s):
+        if int(s[i])%2==0:
+            k+=1
+        i+=1
+    return k
+print(main("5234"))
